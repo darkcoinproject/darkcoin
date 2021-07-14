70fb485c381700af55b952cfc64588d88b6510ee
Munkybooty <shrunkymunky@icloud.com>
2021-07-14 16:48:09 -0400

    Add Chainlock Height/Block info to Debug Window

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -986,7 +986,7 @@ void RPCConsole::updateMasternodeCount()
 
 void RPCConsole::setBestChainLockHash(std::string blockHash)
 {
-    ui->BestChainLockHash->setText(QString(blockHash));
+    ui->BestChainLockHash->setText(QString::fromStdString(blockHash));
 }
 
 void RPCConsole::setBestChainLockHeight(int32_t nHeight)
